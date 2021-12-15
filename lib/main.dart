ecc5163e0ebf31ff490a4178406e6425c8111f6d
hilder <touganhilda@gmail,com>
2021-12-15 16:04:38 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+
+import 'package:chat_app/home_view.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+ 
+void main() {
+  runApp(
+    DevicePreview(
+      builder: (context) => const MyApp()));
+}
+ 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      locale: DevicePreview.locale(context),
+      useInheritedMediaQuery: true,
+      title: 'Contact App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:  HomeView(),
+    );
+  }
+}
